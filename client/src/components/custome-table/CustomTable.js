b438ff6403d576007a1e66347442120137cb8817
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-05-24 09:06:41 +1000

    final feature

--- client/src/components/custome-table/CustomTable.js
+++ client/src/components/custome-table/CustomTable.js
@@ -1,50 +1,67 @@
-import React, {useEffect, useState} from "react";
-import { Alert, Button, Form, ListGroup, Spinner } from "react-bootstrap";
-import {useSelector, useDispatch} from 'react-redux'
-import {fetchExpenses, handleOnDeleteExpenses} from '../../pages/dashboard/dashboardAction'
+import React, { useEffect, useState } from "react";
+import { Alert, Button, ButtonGroup, Form, ListGroup, Spinner } from "react-bootstrap";
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchExpenses, handleOnDeleteExpenses } from '../../pages/dashboard/dashboardAction'
 
 
-export const CustomTable = () => { 
+export const CustomTable = () => {
 	const dispatch = useDispatch()
-	const { expenses, res} = useSelector(state => state.dashboard)
+	const { expenses, res } = useSelector(state => state.dashboard)
 	const [ids, setIds] = useState([])
+	const [display, setDisplay] = useState("all") // income or expenses
 
-	useEffect(()=>{
+	useEffect(() => {
 		dispatch(fetchExpenses())
 		res.status === "success" && setIds([])
-		
+
 	}, [res])
-	
+
 
 	const handleOnDelete = async ids => {
 		if (!window.confirm("Are you sure you want to delete this expense?"))
 			return;
 
 		dispatch(handleOnDeleteExpenses(ids))
-	
+
 	};
 
 
 	const handleOnSelect = (e) => {
-		const {checked, value} =e.target
-		 
-		checked ? setIds([...ids, value]) : setIds(ids.filter((id)=> id !== value))
-	 
-		
+		const { checked, value } = e.target
+
+		checked ? setIds([...ids, value]) : setIds(ids.filter((id) => id !== value))
 	}
+
+	const incomeArg = expenses.filter(expense => expense.type === "income")
+	const expensesArg = expenses.filter(expense => expense.type === "expenses")
+
+	const transactions = {
+		"all": expenses,
+		"income": incomeArg,
+		"expenses": expensesArg,
+	}
+	console.log(transactions[display], display)
 	return (
 		<div className="mt-5 custom-list fs-3">
-			 
+			<div className="btn-group pb-3">
+				<ButtonGroup>
+					<Button onClick={() => setDisplay('all')} variant='primary'>All</Button>
+					<Button onClick={() => setDisplay('income')} variant='info'>Income</Button>
+					<Button onClick={() => setDisplay('expenses')} variant='danger'>Expenses</Button>
+				</ButtonGroup>
+				<div>All the {display} transactions</div>
+			</div>
+
 			<ListGroup>
-				{expenses.map((item, i) => (
+				{transactions[display].map((item, i) => (
 					<ListGroup.Item key={item._id} className="fw-bold">
 						<Form.Group className="mb-3" id="formGridCheckbox">
-						<Form.Check 
-						onClick={handleOnSelect}
-						value={item._id}
-						type="checkbox" label={item.name} />
-					</Form.Group>
-						 
+							<Form.Check
+								onClick={handleOnSelect}
+								value={item._id}
+								type="checkbox" label={item.name} />
+						</Form.Group>
+
 						<span className="cost">
 							{item.type === "expenses" ? "-" : ""}
 							${item.amount}{" "}
@@ -57,13 +74,13 @@ export const CustomTable = () => {
 			</ListGroup>
 
 			<div className="mt-2 text-end">
-			{
-				ids.length > 0 && <Button variant="danger"
-				onClick={()=>handleOnDelete(ids)}
-				>Delete Selected Expenses</Button>
-			}
+				{
+					ids.length > 0 && <Button variant="danger"
+						onClick={() => handleOnDelete(ids)}
+					>Delete Selected Expenses</Button>
+				}
 			</div>
-			
+
 		</div>
 	);
 };
